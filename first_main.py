9b9a04156dd907ef93e05222c44ab633290c7cce
username043852846 <88918828+username043852846@users.noreply.github.com>
2021-08-14 13:42:29 +0900

    Add files via upload

---
+++ first_main.py
@@ -0,0 +1,59 @@
+#dara type
+
+#str,int,float,bool - True, False
+
+#a = 2
+#a = 4
+#==, !=, <, >, <=, >=
+
+# +, -, *, /, %, //
+
+
+
+# loop
+# 소수는 1과 자기자신으로만 나뉘어 떨어짐
+# 변수하나를 더 추가해서
+
+a = int(input('enter the number :'))
+count = 0
+for i in range(2, a//2 + 1):
+    if a % 1 == 0:
+       count = count + 1
+    break
+
+if count == 0:
+    print('prime')
+else:
+    print("not prime")
+
+for i in range(13):
+    if i == 10:
+        continue
+    print(i)
+
+
+# list 리스트
+
+b=[]
+c= [1,2,3,4,5]
+
+b.append(1)
+c.append(2)
+
+import random
+a = random.randrange(1, 100)
+print(a)
+
+a = []
+b = []#  random
+
+for i in  range(100):
+    a.append(i)
+
+for j in range(100):
+    c = random.randrange(1, 100)
+    b.append(c)
+
+# 최대값 찾기 랜덤하게 100개 집어넣건 중에서
+
+# 임원의 수를 하나  이력받고  이거의 약수를 리스트에 집어넣기
