272bc7d4a70253d018e4b68ee099205986e3d839
username043852846 <88918828+username043852846@users.noreply.github.com>
2021-08-28 13:37:52 +0900

    Add files via upload

---
+++ new_file.py
@@ -0,0 +1,58 @@
+# for, while
+
+# for i in range(10):
+#     print('hi')
+
+# i = 0
+# while i < 5:
+#     print('hi', i)
+#     i = i + 1
+
+# while문으로 별찍기
+
+# i = 0
+# while i < 5:
+#     print('*' * i)
+#     i = i + 1
+    # i+= 1
+
+
+# while 문 1,
+# 2,
+# *,
+# 4,
+# 5,
+# *,
+
+# 369실패한거
+# i = 0
+# while i < 100:
+#     i = i + 1
+#     if i%10 %3 ==0:
+#         print('*')
+#     else:
+#         print(i)
+
+# 369
+for i in range(1, 100):
+
+    one = 1 % 10
+    ten = 1 // 10
+
+
+
+# and, or
+
+
+# i = 0
+# while i < 100:
+#     for i in range(2, i//2+ 1):
+#         if i % 1 == 0:
+#         count = count + 1
+#         break
+#         if count == 0:
+#             print('*')
+#         else:
+#             print(i)
+
+
