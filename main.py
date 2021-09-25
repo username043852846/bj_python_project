b43bf335849932292fc3db76492ba0ad9a877af7
username043852846 <88918828+username043852846@users.noreply.github.com>
2021-09-25 13:43:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
+# function , 함수
+
+# def login(x, y):
+#     z = x + y
+#     # 함수를 종료
+#     # 값을 반환한다.
+#     return z
+#
+# result = login(3, 2)
+#
+# print(result)
+
+# 1. 랜덤하게 10개를 집어넣는거를 함수화 시키고 리턴으로 리스트를 받아
+#        make_random_list
+# 2. 리턴 받은 리스트를 입력으로 놓고 여기서 최고 값만 리턴 받아서 출력
+#        find_max_value
+#
+# import random
+# def make_random_list():
+#     a = []
+#     for i in range(10):
+#         random_num = random.randrange(1, 10)
+#         a. append(random_num)
+#     return a
+#
+# def find_max_value(a):
+#     max_value = a[0]
+#     for i in range(len(a)):
+#         if max_value < a[i]:
+#             max_value = a[i]
+#     return max_value
+# print(find_max_value(make_random_list()))
 
 
 
-
-
-
-
-
-
-
-# server, client
-
-#id 중복제거
-#id 8
-#pwd !,@,#
-
-
-# id_list = []
-# pwd_list = []
-
-# for i in range(5):
-#     temp_id = input('enter your id')
-#     for i in range(len(id_list)):
-
-# print('{} x {} = {}'.format(1, 2, 3))
-
-# for i in range(1, 10):
-#     for j in range(1, 10):
-#         print('{} x {} = {}'.format(i, j, i*j))
-
+# 간단한 게임
+    # coin 600원이 주어짐.
+    # 가위 바위 보 s, r, p
+    # 컴퓨터도 600원이 있고 s, r, p 를 선택
+    # 한판당 200원 빵
+    # 그래서 플레이가 돈이 바닥 날떄 까지 게임 진행.
 import random
-a = []
-for i in range(100):
-    b = random.randrange(1, 6)
-    a.append(b)
+user_coin = 600
+com_coin = 600
 
-count_list = [0,0,0,0,0]
-for i in range(len(a)):
-    num = a[i] -1
-    count_list[num] +=1
-print(count_list)
+user_enter = input('enter r, s, p :')
 
+comter_enter = random.randrange(0, 3)
+if comter_enter == 0:
+    comter_enter = 'r'
+elif comter_enter == 1:
+    comter_enter = 's'
+elif comter_enter == 2:
+    comter_enter = 'p'
 
+# 비김
+if user_enter == comter_enter:
+    print('draw')
+#승리
+elif user_enter == 'p' and comter_enter =='r':
+    print('win')
+elif user_enter == 's' and comter_enter == 'p':
+    print('win')
+elif user_enter == 'r' and comter_enter == 's':
+    print('win')
 
-
-
-
-
+#패배
+elif user_enter == 'r' and comter_enter =='p':
+    print('lose')
+elif user_enter == 'p' and comter_enter == 's':
+    print('lose')
+elif user_enter == 's' and comter_enter == 'r':
+    print('lose')
+user_coin(i-100)
 
 
 
