40ec88b44c9fce179632f26a46e1e55dedef197c
username043852846 <88918828+username043852846@users.noreply.github.com>
2021-10-09 13:35:35 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,64 @@
-# 다리 건너기
-# 8번의 찬스
-# 17개
 '''
-1. 다리 생성해하는거.
-2. 진행하는거 코드 짜기
-3. 코드 완료되면 -> 함수화를 어디를 시킬지 결정하고 시킴.
+1. map을 만든다.
+2. 말을 놓는거
+3. 맵을 보여주는거 (print)
+4. 승부 여부
 '''
 
-# import random
-#
-#
-# a = []
-# b = []
-# for i in range(17):
-#     comter_enter = random.randrange(0, 2)
-#     if comter_enter == 0:
-#         comter_enter = '■'
-#         comter_enter_2 = '□'
-#     elif comter_enter == 1:
-#         comter_enter = '□'
-#         comter_enter_2 = '■'
-#     a.append (comter_enter)
-#     b.append (comter_enter_2)
-#
-# print (a)
-# print (b)
+def make_map():
 
+    tic_map = [" " for i in range(9)]
 
-#가장 좋은 코드
-# bridge = []
-# life = 8
-#
-# for i in range(8):
-#     a = random.randrange(0,2)
-#     bridge.append(a)
-# count = 0
-# while life != 0 and count != 7:
-#
-#     ch = int(input('enter your choice 방탄 or dead : (0.1)'))
-#
-#     if bridge[count] == ch:
-#         count += 1
-#         print('살음')
-#     else:
-#         print('you died')
-#         life = life - 1
-#
-#     if count == 7:
-#         print('kkkkkk')
-
-
-# 틱택토
-tic_map = [''for i in range(9)]
-tic_map: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-[0][1][2]
-[3][4][5]
-[6][7][8]
-
-tic_map = []
-for i in range(9):
-    tic_map.append(0)
+    '''
     
+    tic_map = []
+    for i in rnage(9):
+        tic_map.append(0)
+    '''
+
+    return tic_map
+
+tic_map = make_map()
+
+# int(input('enter the number :'))
+
+# [] 안에 O 또는 X 또는 공백이 들어 갈 수 있어야함
+
+
+# 단순히 이렇게 넣으면 ok?
+# 중복 방지
+# 컴퓨터 랜던하게 값을 생성해서 대입
+import random
+
+for i in range(9):
+    print(tic_map)
+    while True:
+        pos = int(input("Enter your location : "))
+        if tic_map[pos] != " ":
+            print('다시')
+        else:
+            tic_map[pos] = "x"
+            break
+    while True:
+        com_pos = int(random.randrange(0,9))
+        print(com_pos)
+        if tic_map[com_pos] != " ":
+            print('rmrj dksla')
+        else:
+            tic_map[com_pos] = "y"
+            break
+
+if  tic_map(0) == tic_map(1) == tic_map(2):
+    tic_map(3) == tic_map(4) == tic_map(5)
+    tic_map(6) == tic_map(7) == tic_map(8)
+    print('win')
+
+else:
+    print('you died')
+
+
+
+
+# 말을 놓는 것.
+# tic_map = put_abatar(tic_map)
+
